29d84cb7897ab37144e53fc10295b26d6338ff44
JOHN NEIL N. COTACTE <johnneil@JOHNs-MacBook-Pro.local>
2024-08-18 19:33:57 +0800

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:bet_pos/config/route/route.dart';
+import 'package:bet_pos/firebase_options.dart';
 import 'package:bet_pos/user/data/di/user_service_locator.dart';
 import 'package:bet_pos/user/presentation/bloc/account_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   setPathUrlStrategy();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   GoRouter.optionURLReflectsImperativeAPIs = true;
 
